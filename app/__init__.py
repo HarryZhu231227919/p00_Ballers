85cc3201c61f7380efd81e14858bc317ce4d18e2
JH Li <jhli@JHs-MacBook-Air.fios-router.home>
2022-11-12 19:20:47 -0500

    dup

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,20 @@ from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
 from flask import session           #facilitate sessions!
 import os                           #facilitate key generation
+import sqlite3
 
-#the conventional way:
-#from flask import Flask, render_template, request
+#DB_FILE = "PAGES"
+#db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+#c = db.cursor()
+
+#command = "CREATE TABLE rosters(name TEXT, mark INTEGER);"
+# test SQL stmt in sqlite3 shell, save as string
+#c.execute(command)    # run SQL statement
+
+#db.commit() #save changes
+#db.close()  #close database
+
+#______________________
 
 app = Flask(__name__)    #create Flask object
 app.secret_key= os.urandom(32) #create random secret key
@@ -23,20 +34,24 @@ correctpw = "correct!"
 @app.route("/", methods=['GET']) #, methods=['GET', 'POST'])
 def disp_loginpage():
     if 'username' in session:   #if already logged in
-        return render_template('response.html', username=session['username'], password=session['password'], request_method = "GET")
+        return render_template('home.html', username=session['username'], password=session['password'], request_method = "GET")
     else:                       #if not logged in, send user to login page
         return render_template('login.html')
 
-    
-@app.route("/auth", methods=['POST'])
+
+@app.route("/login", methods=['POST'])
 def authenticate():
     if request.method == 'POST':
         session['username']= request.form['username']   #remember user in session
         session['password'] = request.form['password']  #remember user in session
         if(correctuser == request.form['username'] and correctpw == request.form['password']):
-            return render_template('response.html', username = request.form['username'], password = request.form['password'], request_method = 'POST') #response to a form submission
+            return render_template('home.html', username = request.form['username'], password = request.form['password'], request_method = 'POST') #response to a form submission
         else:
-            return render_template('login.html', exception =  "Try again") 
+            return render_template('login.html', exception =  "Try again")
+
+@app.route("/register", methods = ['POST'])
+def register():
+    render_template("register.html")
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
